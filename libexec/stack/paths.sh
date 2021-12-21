3086f77759e549efd87cdfab386e4835ba38b1b1
wess <wcope@me.com>
2021-12-21 16:13:14 -0500

    Buggies

--- libexec/stack/paths.sh
+++ libexec/stack/paths.sh
@@ -11,6 +11,10 @@
 
 echo "Loading paths..."
 
+## Homebrew
+export PATH="/opt/homebrew/bin:$PATH"
+export PATH="/opt/homebrew/sbin:$PATH"
+
 ## Rust
 export PATH="${HOME}/.cargo/env:$PATH"
 export DYLD_LIBRARY_PATH=${HOME}/.rustup/toolchains/stable-x86_64-apple-darwin/lib
